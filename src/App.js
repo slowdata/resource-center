8d23ae12dadbaac79f8c3e627224817fbbae56b5
slowdata <slowmotion@outlook.com>
2017-08-07 00:53:27 +0100

    Add drower

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import AppBar from "material-ui/AppBar";
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import "./App.css";
+injectTapEventPlugin();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
   render() {
     return (
       <div className="App">
         <AppBar
           title="Title"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
         />
+        <Drawer 
+          open={this.state.open}
+          docked={false}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
     );
   }
