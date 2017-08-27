b02826f5f2c04c995a808385c88b0815350c0cb7
slowdata <slowmotion@outlook.com>
2017-08-27 17:59:42 +0100

    Drower and style drawer links

--- src/App.js
+++ src/App.js
@@ -1,36 +1,67 @@
-import React, { Component } from "react";
-import AppBar from "material-ui/AppBar";
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import "./App.css";
-injectTapEventPlugin();
+import React, { Component } from 'react'
+import './App.css'
+import MenuItem from 'material-ui/MenuItem'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import fusTheme from './fusTheme'
+
+injectTapEventPlugin()
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {open: false};
+  constructor (props) {
+    super(props)
+    this.state = { open: false }
   }
 
-  render() {
+  handleClose = () => this.setState({ open: false })
+
+  render () {
     return (
-      <div className="App">
-        <AppBar
-          title="Title"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
-        />
-        <Drawer 
-          open={this.state.open}
-          docked={false}
-          onRequestChange={(open) => this.setState({open})}
-        >
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
-        </Drawer>
-      </div>
-    );
+      <BrowserRouter>
+        <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
+          <div>
+            <AppBar
+              title='Title'
+              iconClassNameRight='muidocs-icon-navigation-expand-more'
+              onLeftIconButtonTouchTap={() =>
+                this.setState({ open: !this.state.open })}
+            />
+            <Drawer
+              open={this.state.open}
+              docked={false}
+              onRequestChange={open => this.setState({ open })}
+            >
+              <MenuItem onClick={this.handleClose}>
+                <Link to='/'>Home</Link>
+              </MenuItem>
+              <MenuItem onClick={this.handleClose}>
+                <Link to='/logos'>Logos</Link>
+              </MenuItem>
+              <MenuItem onClick={this.handleClose}>
+                <Link to='/posters'>Posters</Link>
+              </MenuItem>
+              <MenuItem onClick={this.handleClose}>
+                <Link to='/tutorial'>Tutorial</Link>
+              </MenuItem>
+            </Drawer>
+
+            <Route exact path='/' render={() => <h1>Home View</h1>} />
+            <Route
+              exact
+              path='/logos-posters'
+              render={() => <h1>Logos and Posters View</h1>}
+            />
+            <Route exact path='/logos' render={() => <h1>Logos View</h1>} />
+            <Route exact path='/posters' render={() => <h1>Posters View</h1>} />
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MuiThemeProvider } from 'material-ui/styles';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<MuiThemeProvider><App /></MuiThemeProvider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
